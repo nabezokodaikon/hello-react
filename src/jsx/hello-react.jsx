05d106be6a1a5bd156fb7acc18469768d1139c83
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-16 22:44:26 +0900

    State and Lifecycle

--- src/jsx/hello-react.jsx
+++ src/jsx/hello-react.jsx
@@ -1,79 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function formatDate(date) {
-  return date.toLocaleDateString();
-}
-
-function CommentA(props) {
-  return (
-    <div className="Comment">
-      <div className="UserInfo">
-        <img className="Avatar"
-          src={ props.author.avatarUrl }
-          alt={ props.author.name }
-        />
-        <div className="UserInfo-name">
-          { props.author.name }
-      	</div>
-      </div>
-      <div className="Comment-text">
-        { props.text }
-      </div>
-      <div className="Comment-date">
-        { formatDate(props.date) }
-      </div>
-    </div>
-  );
-}
-
-function Avatar(props) {
-  return (
-    <img className="Avatar"
-      src={ props.user.avatarUrl }
-      alt={ props.user.name }
-    />
-  );
-}
-
-function UserInfo(props) {
-  return (
-    <div className="UserInfo">
-      <Avatar user={ props.user } />
-      <div className="UserInfo-name">
-        { props.user.name }
-      </div>
-    </div>
-  );
-}
-
-function CommentB(props) {
-  return (
-    <div className="Comment">
-      <UserInfo user={ props.author } />
-      <div className="Comment-text">
-        { props.text }
-      </div>
-      <div className="Comment-date">
-        { formatDate(props.date) }
-      </div>
-    </div>
-  );
-}
-
-const comment = {
-  date: new Date(),
-  text: "I hope you enjoy learning React!",
-  author: {
-    avatarUrl: "https://avatars1.githubusercontent.com/u/3197942?v=3&s=460",
-    name: "hoge"
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0 };
   }
-};
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+    console.log("componentDidMount");
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+    console.log("componentWillUnmount");
+  }
+  
+  tick() {
+    this.setState((prevState, props) => ({
+      counter: prevState.counter + props.increment
+    }));
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is { this.state.counter }.</h2>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Clock increment={ 1 } />
+        <Clock increment={ 2 } />
+        <Clock increment={ 3 } />
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
-  <CommentB
-    author={ comment.author }
-    text={ comment.text }
-    date={ comment.date } />,
+  <App />,
   document.getElementById("root")
 );
