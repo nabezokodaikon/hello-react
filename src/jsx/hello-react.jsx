dee2ba03e5fafe9579be7ca00736e6cb4f1deb2c
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-17 23:19:22 +0900

    Add Conditional Rendering

--- src/jsx/hello-react.jsx
+++ src/jsx/hello-react.jsx
@@ -1,64 +1,164 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Button extends React.Component {
-  print() {
-    console.log("Button click!");
+class Greeting extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  user() {
+    return <h1>Welcome back!</h1>;
+  }
+
+  guest() {
+    return <h1>Please sign up.</h1>;
   }
 
   render() {
-    return (
-      <button onClick={this.print}>
-        Click
-      </button>
-    );  
+    const isLoggedIn = this.props.isLoggedIn;
+    if (isLoggedIn) {
+      return this.user();
+    } else {
+      return this.guest();
+    }
   }
 }
 
-class Link extends React.Component {
-  print(e) {
-    e.preventDefault();
-    console.log("Link click!");
+class LoginButton extends React.Component {
+  constructor(props) {
+    super(props);
   }
 
   render() {
     return (
-      <a href="#" onClick={this.print}>
-        Click me.
-      </a>
+      <button onClick={this.props.onClick}>
+        Login
+      </button>
     );
   }
 }
 
-class Toggle extends React.Component {
+class LogoutButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { isToggleOn: true };
-    this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick() {
+  render() {
+    return (
+      <button onClick={this.props.onClick}>
+        Logout
+      </button>
+    );
+  }
+}
+
+class LoginControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoggedIn: false };
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+  }
+
+  handleLoginClick() {
+    this.setState({ isLoggedIn: true });
+  }
+
+  handleLogoutClick() {
+    this.setState({ isLoggedIn: false });
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    if (isLoggedIn) {
+      return (
+        <div>
+          <Greeting isLoggedIn={isLoggedIn} />
+          <LogoutButton onClick={this.handleLogoutClick} />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Greeting isLoggedIn={isLoggedIn} />
+          <LoginButton onClick={this.handleLoginClick} />
+        </div>
+      );
+    }
+  }
+}
+
+class Mailbox extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const unreadMessages = this.props.unreadMessages;
+    return (
+      <div>
+        <h1>Hello!</h1>
+        {unreadMessages.length > 0 &&
+          <h2>
+            You have {unreadMessages.length} unread messages.
+          </h2>
+        }
+      </div>
+    );
+  }
+}
+
+class WarningBanner extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    if (this.props.warn) {
+      return null;
+    } else {
+      return (
+        <div>
+          Warning!
+        </div>
+      );
+    }
+  }
+}
+
+class Page extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { showWarning: true };
+    this.handleToggleClick = this.handleToggleClick.bind(this);
+  }
+
+  handleToggleClick() {
     this.setState(prevState => ({
-      isToggleOn: !prevState.isToggleOn
+      showWarning: !prevState.showWarning
     }));
   }
 
   render() {
     return (
-      <button onClick={this.handleClick}>
-        {this.state.isToggleOn ? "ON" : "OFF"}
-      </button>
+      <div>
+        <WarningBanner warn={this.state.showWarning} />
+        <button onClick={this.handleToggleClick}>
+          {this.state.showWarning ? "Hide" : "Show" }
+        </button>
+      </div>
     );
   }
 }
 
 class App extends React.Component {
   render() {
+    const messages = ["a"];
     return (
       <div>
-        <Button />
-        <Link />
-        <Toggle />
+        <LoginControl />
+        <Mailbox unreadMessages={messages} />
+        <Page />
       </div>
     );
   }
