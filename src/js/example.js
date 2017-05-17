39841734e30c86c9fd80e80c7de4ae4b60c5bd50
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-17 21:17:02 +0900

    あきらめた

--- src/js/example.js
+++ src/js/example.js
@@ -1,26 +1,53 @@
 "use strict"
 
-// const readline = require('readline'),
-// rl = readline.createInterface(process.stdin, process.stdout);
-// rl.on("line", cmd => {
-  // console.log("> " + cmd);
-// })
+let counter = 0;
 
-function hoge(i) {
-  console.log(i);
-  if (i > 100000) {
-    return i;
-  } else {
-    return hoge(i + 1);
+const readline = require('readline');
+const rl = readline.createInterface(process.stdin, process.stdout);
+rl.on("line", cmd => {
+  counter += 1;
+  const text = "> ["+ counter + "] " + cmd;
+  console.log(text);
+});
+
+class IO {
+  constructor(f) {
+    this.f = f;
+  }
+
+  run() {
+    return this.f();
+  }
+
+  map(f) {
+    return new IO(() => f(this.run()));
+  }
+
+  flatMap(f) {
+    return new IO(() => f(this.run()).run);
   }
 }
 
-hoge(1);
-// [1,2,3].map(n => n + 1);
+function printLine(msg) {
+  return new IO(() => console.log(msg));
+}
 
-// function sum_rec(n, a) {
-  // if (n == 0) return a;
-  // return sum_rec(n - 1, a + n);
+const a = printLine("hello");
+// a.run();
+const b = a.map(i => console.log(i));
+// b.run();
+const c = a.flatMap(i => new IO(() => console.log(i)));
+c.run();
+
+// function hoge(i) {
+  // console.log(i);
+  // if (i > 100000) {
+    // return i;
+  // } else {
+    // return hoge(i + 1);
+  // }
 // }
 
-// console.log(sum_rec(100000, 0));
+// hoge(1);
+// [1,2,3].map(n => n + 1);
+
