004aef1e5e1435dd5572b21d4361dd2fba97e519
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-17 21:55:30 +0900

    Add Handling Events

--- src/jsx/hello-react.jsx
+++ src/jsx/hello-react.jsx
@@ -1,35 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Clock extends React.Component {
+class Button extends React.Component {
+  print() {
+    console.log("Button click!");
+  }
+
+  render() {
+    return (
+      <button onClick={this.print}>
+        Click
+      </button>
+    );  
+  }
+}
+
+class Link extends React.Component {
+  print(e) {
+    e.preventDefault();
+    console.log("Link click!");
+  }
+
+  render() {
+    return (
+      <a href="#" onClick={this.print}>
+        Click me.
+      </a>
+    );
+  }
+}
+
+class Toggle extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { counter: 0 };
-    hoge(1);
+    this.state = { isToggleOn: true };
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  componentDidMount() {
-    this.timerID = setInterval(() => this.tick(), 1000);
-    console.log("componentDidMount");
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-    console.log("componentWillUnmount");
-  }
-  
-  tick() {
-    this.setState((prevState, props) => ({
-      counter: prevState.counter + props.increment
+  handleClick() {
+    this.setState(prevState => ({
+      isToggleOn: !prevState.isToggleOn
     }));
   }
 
   render() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is { this.state.counter }.</h2>
-      </div>
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? "ON" : "OFF"}
+      </button>
     );
   }
 }
@@ -38,9 +56,9 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Clock increment={ 1 } />
-        <Clock increment={ 2 } />
-        <Clock increment={ 3 } />
+        <Button />
+        <Link />
+        <Toggle />
       </div>
     );
   }
