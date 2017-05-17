cf1c0795715b392346663dd59a631bc7569a4649
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-18 06:18:09 +0900

    Add code of rendering multiple components

--- src/jsx/lists-and-keys.jsx
+++ src/jsx/lists-and-keys.jsx
@@ -1,10 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const numbers = [1, 2, 3, 4, 5];
+    const listItems = numbers.map(n => 
+      <li>{n * 2}</li>
+    );
+    return (
+      <ul>
+        {listItems}
+      </ul>
+    );
+  }
+}
+
 class App extends React.Component {
   render() {
     return (
       <div>
+        <List />
       </div>
     );
   }
