fd7661bf443d971a452c697d1cc13788dcdb2b69
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-18 10:39:56 +0900

    Add keys must only be unique among siblings

--- src/jsx/lists-and-keys.jsx
+++ src/jsx/lists-and-keys.jsx
@@ -50,14 +50,68 @@ class TodoList extends React.Component {
   }
 }
 
+class Post extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>{this.props.id}</h2>
+        <h3>{this.props.title}</h3>
+        <p>{this.props.content}</p>
+      </div>
+    );
+  }
+}
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const sidebar = (
+      <ul>
+        {this.props.posts.map(post =>
+          <li key={post.id}>
+            {post.title}
+          </li>
+        )}
+      </ul>
+    );
+    const content = this.props.posts.map(post =>
+      <Post
+        key={post.id}
+        id={post.id}
+        title={post.title}
+        content={post.content}
+      />
+    );
+    return (
+      <div>
+        {sidebar}
+        <hr />
+        {content}
+      </div>
+    );
+  }
+}
+
 class App extends React.Component {
   render() {
     const numbers = [1, 2, 3, 4, 5];
     const todos = ["Coding", "Build", "Test", "Publish"];
+    const posts = [
+      { id: 1, title: 'Hello World', content: 'Welcome to learning React!' },
+      { id: 2, title: 'Installation', content: 'You can install React from npm.' }
+    ];
     return (
       <div>
         <NumberList numbers={numbers} />
         <TodoList todos={todos} />
+        <Blog posts={posts} />
       </div>
     );
   }
