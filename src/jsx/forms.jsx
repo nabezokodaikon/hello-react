da7a74ddad76879ff212909735fb04dcce726843
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-18 14:11:27 +0900

    Add the textarea tag

--- src/jsx/forms.jsx
+++ src/jsx/forms.jsx
@@ -33,10 +33,47 @@ class NameForm extends React.Component {
   }
 }
 
+class EssayForm extends React.Component {
+  constructor(prop) {
+    super(prop);
+    this.state = {
+      value: "Please write an essay about your favorite DOM element."
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  handleSubmit(e) {
+    alert("An essay was submitted: " + this.state.value);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <textarea
+            value={this.state.value}
+            onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
 class App extends React.Component {
   render() {
     return (
-      <NameForm />
+      <div>
+        <NameForm />
+        <EssayForm />
+      </div>
     );
   }
 }
