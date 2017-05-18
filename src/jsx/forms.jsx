c0be18595ea98257794b656e4043faea6d77c85e
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-18 16:06:54 +0900

    Add handling multiple inputs

--- src/jsx/forms.jsx
+++ src/jsx/forms.jsx
@@ -102,6 +102,60 @@ class FlavorForm extends React.Component {
   }
 }
 
+class Reservation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isGoing: true,
+      numberOfGuests: 2
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleInputChange(e) {
+    const target = e.target;
+    const value =
+      target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(e) {
+    console.log("isGoing: " + this.state.isGoing);
+    console.log("numberOfGuests: " + this.state.numberOfGuests);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+        Is going:
+        <input 
+          name="isGoing"
+          type="checkbox"
+          checked={this.state.isGoing}
+          onChange={this.handleInputChange} />
+        </label>
+        <br />
+        <label>
+          Number of guests:
+          <input
+            name="numberOfGuests"
+            type="number"
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
 class App extends React.Component {
   render() {
     return (
@@ -109,6 +163,7 @@ class App extends React.Component {
         <NameForm />
         <EssayForm />
         <FlavorForm />
+        <Reservation />
       </div>
     );
   }
