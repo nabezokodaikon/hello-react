0c33cbe98210765670681d43b9275cae8446f96b
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-18 14:31:14 +0900

    Add the select tag

--- src/jsx/forms.jsx
+++ src/jsx/forms.jsx
@@ -67,12 +67,48 @@ class EssayForm extends React.Component {
   }
 }
 
+class FlavorForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "coconut" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
+  handleSubmit(e) {
+    alert("Your favorite flavor is: " + this.state.value);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Pick your favorite La Croix flavor:
+          <select value={this.state.value} onChange={this.handleChange}>
+            <option value="grapefruit">Grapefruit</option>
+            <option value="lime">Lime</option>
+            <option value="coconut">Coconut</option>
+            <option value="mango">Mango</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
 class App extends React.Component {
   render() {
     return (
       <div>
         <NameForm />
         <EssayForm />
+        <FlavorForm />
       </div>
     );
   }
