6d749507506656323965e0de45ed431f7a85cdb8
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-18 13:59:35 +0900

    Add Controlled Components

--- src/jsx/forms.jsx
+++ src/jsx/forms.jsx
@@ -1,9 +1,43 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+class NameForm extends React.Component {
+  constructor(prop) {
+    super(prop);
+    this.state = { value: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value.toUpperCase() });
+  }
+
+  handleSubmit(e) {
+    alert("A name was submit: " + this.state.value);
+    e.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input type="text"
+                 value={this.state.value}
+                 onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
+
 class App extends React.Component {
   render() {
-    return null;
+    return (
+      <NameForm />
+    );
   }
 }
 
