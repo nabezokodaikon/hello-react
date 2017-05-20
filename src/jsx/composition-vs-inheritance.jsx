a56e61a788327b779d158e0dbe4899f2f2f483a7
nabezokodaikon <nabezoko.daikon@gmail.com>
2017-05-20 12:30:08 +0900

    Add "Specialization" code

--- src/jsx/composition-vs-inheritance.jsx
+++ src/jsx/composition-vs-inheritance.jsx
@@ -15,7 +15,7 @@ class FancyBorder extends React.Component {
   }
 }
 
-class WelcomeDialog extends React.Component {
+class Dialog extends React.Component {
   constructor(props) {
     super(props);
   }
@@ -24,12 +24,58 @@ class WelcomeDialog extends React.Component {
     return (
       <FancyBorder color="blue">
         <h1 className="Dialog-title">
-          Welcome
+          {this.props.title} 
         </h1>
         <p className="Dialog-message">
-          Thank you for visiting our spacecraft!
+          {this.props.message}
         </p>
+        {this.props.children}
       </FancyBorder>
+    );
+  }
+}
+
+class SignUpDialog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { login: "" }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSignUp = this.handleSignUp.bind(this);
+  }
+
+  render() {
+    return (
+      <Dialog title="Mars Exploration Program"
+              message="How should we refer to you?">
+        <input value={this.state.login}
+               onChange={this.handleChange} />
+        <button onClick={this.handleSignUp}>
+          Sign Me Up!
+        </button>
+      </Dialog>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({ login: e.target.value });
+  }
+
+  handleSignUp() {
+    alert(`Welcome aboard, ${this.state.login}`);
+  }
+}
+
+class WelcomeDialog extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Dialog 
+        title="Welcome"
+        message="Thank you for visiting our spacecraft!"
+      />
     );
   }
 }
@@ -86,6 +132,7 @@ class App extends React.Component {
           left={<Contacts />}
           right={<Chat />}
         />
+        <SignUpDialog />
       </div>
     );
   }
